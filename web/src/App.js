3c8ba7c718a2c02b5d5d9a6ca1b21ab1c316e37a
Luc Saunders <lucsaunders@gmail.com>
2019-06-19 11:44:40 -0400

    Add initial skeleton file structure

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = props => {
+  return (
+    <div className="App">
+      <header className="App-header">Sherpa Book Review</header>
+    </div>
+  );
+};
+
+export default App;
