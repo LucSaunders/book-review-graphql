6357c263b0cb5ac7085678601346006ac4b31835
LucSaunders <lucsaunders@gmail.com>
2019-06-23 11:58:45 -0400

    Prep server. Add express, graphqlExpress, graphiql, cors, and bodyParser to handle requests.

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,7 @@
+import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import { makeExecutableSchema } from 'graphql-tools';
 import { graphql } from 'graphql';
 
@@ -20,8 +24,28 @@ const resolvers = {
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const query = process.argv[2];
+const app = express();
 
-graphql(schema, query).then(result => {
-  console.log(JSON.stringify(result, null, 2));
+// Enable use of middleware.
+// Call cors middleware function to add cross-origin recourse-sharing support to the http server.
+app.use(cors());
+
+// Set up routing rule for single graphql endpoint, so when http requests arrive at the /graphql url, the following middleware will handle the request.
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  // Schema must be passed into graphqlExpress
+  graphqlExpress({ schema })
+);
+
+// Set up graphiql
+app.use(
+  '/graphiql',
+  // graphiqlExpress middleware requires config object with property of endpointURL
+  graphiqlExpress({ endpointURL: '/graphql' })
+);
+
+// Set port with callback to direct to graphiql queries.
+app.listen(4000, () => {
+  console.log('Go to http://localhost:4000/graphiql to run queries.');
 });
